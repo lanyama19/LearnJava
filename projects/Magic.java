423b4eb445355ba5cda37ca0f74c7c9b7c10b621
frankM <laneyamar@gmail.com>
2022-10-04 17:11:01 +0800

    new commit

--- projects/Magic.java
+++ projects/Magic.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Magic {
+public class Magic {
+    public static void main(String[] args) {
+        // Original number
+        int myNumber = 42;
+        int stepOne = myNumber * myNumber;
+        int stepTwo = stepOne + myNumber;
+        int stepThree = stepTwo/myNumber;
+        int stepFour = stepThree + 17;
+        int stepFive = stepFour - myNumber;
+        int stepSix = stepFive/6;
+        System.out.println(stepSix);
+    }
 }
